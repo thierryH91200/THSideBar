5aada496ee7628bce215f92f570474b0a69731b3
Adrhs <hamel>
2017-10-01 07:01:47 +0200

    Initial Commit

---
+++ SideBarDemo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SideBarDemo
+//
+//  Created by thierryH24 on 01/10/2017.
+//  Copyright © 2017 thierryH24. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
